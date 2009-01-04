b32e8b90c58d48b7f5db431eaa2b8cf9e9dae11d
robnyman <robnyman@b7863c80-d6db-11dd-a412-a1792178251d>
2009-01-04 19:58:52 +0000

    Selection color change, improved preloading, name case changed for backwards compatibility

--- add-on/chrome/content/flickrGalleryPlus.js
+++ add-on/chrome/content/flickrGalleryPlus.js
@@ -11,19 +11,20 @@ var flickrGalleryPlus = function () {
 		prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
 		
 	init = function () {
-		var autoRun = prefManager.getBoolPref("extensions.flickrGalleryPlus.autorun");
-		if (autoRun && /flickr\.com/i.test(content.document.domain)) {
-			// gBrowser.tabContainer.addEventListener("TabSelect", function () {
-			// 				stopSlideshow();
-			// 			}, false);
-			
+		var autoRun = prefManager.getBoolPref("extensions.flickrgalleryplus.autorun"),
+			mainImage;
+		if (autoRun && /flickr\.com\/photos\/[^\/]+\/sets\//i.test(content.location.href)) {
+			mainImage = content.document.getElementById("primary_photo_img");
+			if (mainImage) {
+				mainImage.style.visibility = "hidden";
+			}
 			run(true);
 		}
 	};
 	
 	run = function (atLoad) {
 		var state = getState(),
-			autoRun = prefManager.getBoolPref("extensions.flickrGalleryPlus.autorun"),
+			autoRun = prefManager.getBoolPref("extensions.flickrgalleryplus.autorun"),
 			mainPhoto = content.document.getElementById("primary_photo_img"),
 			head = content.document.getElementsByTagName("head")[0],
 			script,
@@ -38,18 +39,18 @@ var flickrGalleryPlus = function () {
 				stopSlideshow();
 			}, false);
 			
-			var link = content.document.createElement("link").wrappedJSObject;
+			link = content.document.createElement("link").wrappedJSObject;
 			link.type = "text/css";
 			link.rel = "stylesheet";
-			link.href = "chrome://flickrGalleryPlus/skin/flickrGalleryPlus.css";
+			link.href = "chrome://flickrgalleryplus/skin/flickrGalleryPlus.css";
 			head.appendChild(link);
 			
 			script = content.document.createElement("script").wrappedJSObject;
-			script.src = "chrome://flickrGalleryPlus/content/jquery-1.2.6.min.js";
+			script.src = "chrome://flickrgalleryplus/content/jquery-1.2.6.min.js";
 			script.type = "text/javascript";
 			head.appendChild(script);
 			
-			slideTime = parseInt(prefManager.getIntPref("extensions.flickrGalleryPlus.slideshowSlideTime"), 10);
+			slideTime = parseInt(prefManager.getIntPref("extensions.flickrgalleryplus.slideshowSlideTime"), 10);
 			
 			script.onload = applyGallery;
 		}
@@ -90,24 +91,23 @@ var flickrGalleryPlus = function () {
 			state.slideshowRunning = false;
 			
 			stopSlideshow();
-			//this.setStatusBar();
 		}
 	};
 	
 	setStatusBar = function () {
 		var state = this.getState(),
-			statusIcon = "chrome://flickrGalleryPlus/skin/",
+			statusIcon = "chrome://flickrgalleryplus/skin/",
 			statusText;
 			
 		if(state && state.hasRun) {
 			statusIcon += "status-bar.png";
 			statusText = "Disable Flickr Gallery Plus!";
-			prefManager.setBoolPref("extensions.flickrGalleryPlus.autorun", true);
+			prefManager.setBoolPref("extensions.flickrgalleryplus.autorun", true);
 		}
 		else {
 			statusIcon += "status-bar-disabled.png";
 			statusText = "Activate Flickr Gallery Plus!";
-			prefManager.setBoolPref("extensions.flickrGalleryPlus.autorun", false);
+			prefManager.setBoolPref("extensions.flickrgalleryplus.autorun", false);
 		}
 		statusBarButton.setAttribute("src", statusIcon);
 		statusBarButton.setAttribute("tooltiptext", statusText);
@@ -153,13 +153,14 @@ var flickrGalleryPlus = function () {
 		state.primaryPhoto.removeAttr("width");
 		state.primaryPhoto.removeAttr("height");
 		state.primaryPhoto.load(function () {
+			this.style.visibility = "visible";
 			clearTimeout(state.timer);
 			state.loadingImage.hide();
 		});
 		
 		var thumbnailElms = $("#setThumbs .pc_img"),
-			preloadImages = prefManager.getBoolPref("extensions.flickrGalleryPlus.preloadImages");
-		for (var i=0, il=thumbnailElms.length, thumbnail, preload; i<il; i++) {
+			preloadImages = prefManager.getBoolPref("extensions.flickrgalleryplus.preloadImages");
+		for (var i=0, il=thumbnailElms.length, thumbnail, src; i<il; i++) {
 			thumbnail = $(thumbnailElms[i]);
 			src = thumbnail[0].src.replace(fileNameReplace, "$1");
 			state.thumbnails.push({
@@ -168,17 +169,13 @@ var flickrGalleryPlus = function () {
 				title : thumbnail[0].alt,
 				href : thumbnail.parent("a").attr("href")
 			});
-			if (preloadImages) {
-				preload = content.document.createElement("img").wrappedJSObject;
-				preload.setAttribute("src", src);
-			}
 			thumbnail.click(function (index) {
 				return function (evt) {
 					setImage(index);
 					return false;
-				}
+				};
 			}(i));
-		};
+		}
 		
 		setImage(0);
 		
@@ -198,6 +195,13 @@ var flickrGalleryPlus = function () {
 		});
 		
 		setStatusBar();
+		
+		if (preloadImages) {
+			for (var j=0, jl=state.thumbnails.length, preload; j<jl; j++) {
+				preload = content.document.createElement("img").wrappedJSObject;
+				preload.setAttribute("src", state.thumbnails[j].src);
+			}
+		}
 	};
 	
 	setImage = function (index) {
@@ -281,6 +285,6 @@ flickrGalleryPlusWrapper = {
 
 window.addEventListener("load", function () {
 	gBrowser.addEventListener("DOMContentLoaded", function () {
-		flickrGalleryPlus.init.apply(flickrGalleryPlus, arguments);
+		flickrGalleryPlus.init();
 	}, false);
 }, false);
