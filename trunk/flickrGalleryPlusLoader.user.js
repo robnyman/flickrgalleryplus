f58da949267b74baab9bf121713798fe33f74a7a
robnyman <robnyman@b7863c80-d6db-11dd-a412-a1792178251d>
2009-01-05 02:03:45 +0000

    Loader file

---
+++ trunk/flickrGalleryPlusLoader.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name           Flickr Gallery Plus!
+// @namespace      robertnyman.com
+// @description    Improve Flickr photo set viewing
+// @include        http://www.flickr.com/photos/robertnyman/sets/*/
+// @include        http://flickr.com/photos/robertnyman/sets/*/
+// ==/UserScript==
+var flickrGalleryPlusLoader = function () {
+	var scripLocation = "http://flickrgalleryplus.googlecode.com/svn/branches/live/flickrGalleryPlus.js";
+		
+	init = function () {
+		var head = content.document.getElementsByTagName("head")[0],
+			script;
+		if (head) {
+			script = content.document.createElement("script").wrappedJSObject;
+			script.src = scripLocation;
+			script.type = "text/javascript";
+			head.appendChild(script);
+		}
+	};
+	
+	return {
+		init : init
+	};
+}();
+flickrGalleryPlusLoader.init();
