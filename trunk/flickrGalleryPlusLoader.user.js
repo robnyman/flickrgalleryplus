a67a47d6c6c8414aed784258bb858b06fe283ef1
robnyman <robnyman@b7863c80-d6db-11dd-a412-a1792178251d>
2009-01-06 20:44:50 +0000

    Fixed default URLs for the Greasemonkey script loader

--- trunk/flickrGalleryPlusLoader.user.js
+++ trunk/flickrGalleryPlusLoader.user.js
@@ -2,8 +2,8 @@
 // @name           Flickr Gallery Plus!
 // @namespace      robertnyman.com
 // @description    Improve Flickr photo set viewing
-// @include        http://www.flickr.com/photos/robertnyman/sets/*/
-// @include        http://flickr.com/photos/robertnyman/sets/*/
+// @include        http://www.flickr.com/photos/*/sets/*/
+// @include        http://flickr.com/photos/*/sets/*/
 // ==/UserScript==
 var flickrGalleryPlusLoader = function () {
 	var scripLocation = "http://flickrgalleryplus.googlecode.com/svn/branches/live/flickrGalleryPlus.js";
