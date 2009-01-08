7d718333135c2bf5a157c6bff73886de8c9e55cf
robnyman <robnyman@b7863c80-d6db-11dd-a412-a1792178251d>
2009-01-08 22:14:19 +0000

    Created universal loader for Firefox, Safari, Chrome and Opera

--- trunk/flickrGalleryPlusLoader.user.js
+++ trunk/flickrGalleryPlusLoader.user.js
@@ -9,10 +9,10 @@ var flickrGalleryPlusLoader = function () {
 	var scripLocation = "http://flickrgalleryplus.googlecode.com/svn/branches/live/flickrGalleryPlus.js";
 		
 	init = function () {
-		var head = content.document.getElementsByTagName("head")[0],
+		var head = document.getElementsByTagName("head")[0],
 			script;
 		if (head) {
-			script = content.document.createElement("script").wrappedJSObject;
+			script = document.createElement("script");
 			script.src = scripLocation;
 			script.type = "text/javascript";
 			head.appendChild(script);
